9b8052115be1d50654545aa4f8cef3bc406e634d
liderako <trotillostudio@gmail.com>
2018-11-22 19:20:33 +0200

    update project

---
+++ android/src/com/student/asvirido/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.student.asvirido.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.student.asvirido.game.Main;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Main(), config);
+	}
+}
