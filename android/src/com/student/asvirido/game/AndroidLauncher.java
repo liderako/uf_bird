4746b0b49802e4bdbd9da51e5be59e8251f10ff6
liderako <trotillostudio@gmail.com>
2018-12-09 20:55:23 +0200

    update project last

--- android/src/com/student/asvirido/game/AndroidLauncher.java
+++ android/src/com/student/asvirido/game/AndroidLauncher.java
@@ -10,6 +10,6 @@ public class AndroidLauncher extends AndroidApplication {
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new Main(), config);
+		initialize(new ZBGame(), config);
 	}
 }
